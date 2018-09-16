b33f4a6ca7b23bdfa7f3500c9003166f65ace45b
Moritz Berthold <Moritz@Moritzs-MacBook-Pro.local>
2018-09-16 21:16:23 +0200

    double channel

--- 0_preprocessing.py
+++ 0_preprocessing.py
@@ -8,19 +8,24 @@ from numpy import genfromtxt
 from scipy import misc
 import glob
 import math
-
+import cv2
 
 plot = False
-bounding_box_size = 64
+bounding_box_size = 32
 save_crops = True
 dataset = "A" # A or H
+dataset2 = "H"
 include_negatives=True
-sample_ratio=2
+sample_ratio=1
 hold_back_test_data = True
+double_channel=True
 
+dimensions = 3
+if double_channel:
+    dimensions = 6
 def crop_image(image, cell_center, bounding_box_size, cell_array):
     margin = np.round(bounding_box_size/2).astype(int)
-    pad_image = np.pad(image[:,:,:3], ((margin,margin),(margin,margin),(0,0)), mode="constant", constant_values=0)
+    pad_image = np.pad(image[:,:,:dimensions], ((margin,margin),(margin,margin),(0,0)), mode="constant", constant_values=0)
     crop = pad_image[cell_center[0]:cell_center[0]+2*margin, cell_center[1]:cell_center[1]+2*margin,:]
     mask = np.zeros_like(crop[:,:,0])
 
@@ -36,7 +41,7 @@ def crop_image(image, cell_center, bounding_box_size, cell_array):
 
 def crop_neg(image, cell_center, bounding_box_size):
     margin = np.round(bounding_box_size/2).astype(int)
-    pad_image = np.pad(image[:,:,:3], ((margin,margin),(margin,margin),(0,0)), mode="constant", constant_values=0)
+    pad_image = np.pad(image[:,:,:dimensions], ((margin,margin),(margin,margin),(0,0)), mode="constant", constant_values=0)
     neg = pad_image[cell_center[0]:cell_center[0]+2*margin, cell_center[1]:cell_center[1]+2*margin,:]
     neg_mask = np.zeros_like(neg[:,:,0])
     del pad_image, image
@@ -45,7 +50,7 @@ def crop_neg(image, cell_center, bounding_box_size):
 
 data_path = "/Users/Moritz/Desktop/zeiss/data/"
 # Load A - Channel
-a_channel_cells = np.empty([0, bounding_box_size, bounding_box_size, 3])
+a_channel_cells = np.empty([0, bounding_box_size, bounding_box_size, dimensions])
 a_channel_masks = np.empty([0, bounding_box_size, bounding_box_size])
 a_channel_annotations = []
 if hold_back_test_data:
@@ -56,6 +61,10 @@ for file_path in filelist:
     for csv_path in sorted(glob.glob(file_path + "*.csv")):
         all_annotation_txt = np.genfromtxt(csv_path, dtype = 'str', comments='#', delimiter="',\n'", skip_header=0, skip_footer=0, converters=None, missing_values=None, filling_values=None, usecols=None, names=None, excludelist=None, deletechars=None, replace_space='_', autostrip=False, case_sensitive=True, defaultfmt='f%i', unpack=None, usemask=False, loose=True, invalid_raise=True, max_rows=None, encoding='bytes')
         image = misc.imread(csv_path.replace(".csv", ".png"))/255
+        if double_channel:
+            image2 = misc.imread(csv_path.replace(".csv", ".png").replace(dataset, dataset2))/255
+            image2 = cv2.resize(image2, (image.shape[0], image.shape[1]), interpolation = cv2.INTER_LINEAR)
+            image = np.concatenate((image, image2), axis=-1)
         cells_in_image = []
         for cell_string in np.nditer(all_annotation_txt):
             cell_array = np.fromstring(np.array2string(cell_string).strip("'"), dtype=int, sep=",")
@@ -73,8 +82,10 @@ for file_path in filelist:
             del(mask, crop)
         a_channel_annotations.append(cells_in_image)
 
+
+
 if include_negatives:
-    negs = np.empty([0, bounding_box_size, bounding_box_size, 3])
+    negs = np.empty([0, bounding_box_size, bounding_box_size, dimensions])
     negs_masks = np.empty([0, bounding_box_size, bounding_box_size])
     if hold_back_test_data:
         filelist = sorted(glob.glob(data_path + dataset + "0*_v2/*.png"))[:-1]
@@ -82,6 +93,10 @@ if include_negatives:
         filelist = sorted(glob.glob(data_path + dataset + "0*_v2/*.png"))
     for image_path in filelist:
             image = misc.imread(image_path)/255
+            if double_channel:
+                image2 = misc.imread(image_path.replace(dataset, dataset2))/255
+                image2 = cv2.resize(image2, (image.shape[0], image.shape[1]), interpolation = cv2.INTER_LINEAR)
+                image = np.concatenate((image, image2), axis=-1)
             negs_in_image = []
             n = math.ceil(320*sample_ratio / 80) * 2 # round to even number
             neg_centers = np.random.randint(0, 2048, n*2)
@@ -101,15 +116,13 @@ if include_negatives:
     a_channel_masks = np.vstack((a_channel_masks, negs_masks))
 
 
-
 if save_crops:
-    filename = data_path + "preprocessed/" + "cropsize=" + str(bounding_box_size)+ "scanner=" +dataset+"include_negatives="+str(include_negatives)+"ratio="+str(sample_ratio)+"hb="+str(hold_back_test_data)
+    filename = data_path + "preprocessed/" + "cropsize=" + str(bounding_box_size)+ "scanner=" +dataset+"include_negatives="+str(include_negatives)+"ratio="+str(sample_ratio)+"hb="+str(hold_back_test_data) + "double_channel="+str(double_channel)
     np.save(filename+ ".npy", a_channel_cells, allow_pickle=True, fix_imports=True)
     np.save(filename+ "masks.npy", a_channel_masks, allow_pickle=True, fix_imports=True)
 
-
-        #print(len(cells_in_image))
 code.interact(local=dict(globals(), **locals()))
+        #print(len(cells_in_image))
     #    a_channel_annotations.append(np.asarray(cells_in_image))
     #for image_path in glob.glob(file_path + "*.jpg"):
     #    image = misc.imread(image_path)/255
@@ -125,7 +138,7 @@ if plot:
     plotHistogram(y_diameters,'Pixel','Häufigkeit',"Y-Durchmesser")
     plotHistogram(sizes,'Total-Pixel-Count','Häufigkeit',"Cell-Size")
     plt.show()
-    code.interact(local=dict(globals(), **locals()))
+
 
 plt.imshow(a_channel_images[0,:,:,:])
 plt.show()
