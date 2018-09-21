8edb177ce084d8da12c617cfa4054a1f80eee1cd
Moritz Berthold <Moritz@Moritzs-MacBook-Pro.local>
2018-09-21 09:16:22 +0200

    Readmy improvements

---
+++ 3_generate_data.py
@@ -0,0 +1,168 @@
+# Script to reduce false positives
+# from algorithms import *
+from plot_lib import *
+from models import *
+from sklearn.model_selection import cross_val_score, StratifiedKFold, train_test_split
+from sklearn.feature_selection import RFE
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import KFold
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import CSVLogger
+from keras.models import load_model
+from sklearn.metrics import log_loss
+from keras.callbacks import LearningRateScheduler
+from keras.preprocessing.image import ImageDataGenerator
+import keras.backend as K
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from sklearn.metrics import jaccard_similarity_score
+import scipy.ndimage as nd
+import cv2
+import numpy as np
+import _pickle as cPickle
+import code
+import glob
+from scipy import misc
+
+import os
+
+
+# Aim: crop 64x64 flasely and correctly segmented masks. [n,x,y,channels+probability] = [n, 64,64,4]
+
+dataset = "A"
+bounding_box_size = 32
+stepSize=4
+minimum_cellsize = 300
+maximum_cellsize = 2800
+thresholding_probabilities = True
+plot=False
+
+def dice_coef_here(y_true, y_pred):
+    y_true_f = np.concatenate(y_true)
+    y_pred_f = np.concatenate(y_pred)
+    intersection = np.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (np.sum(y_true_f*y_true_f) + np.sum(y_pred_f*y_pred_f) + smooth)
+
+
+def dice_coef_loss_here(y_true, y_pred):
+    return 1.-dice_coef_here(y_true, y_pred)
+
+
+checkpoint_path = "/Users/Moritz/Desktop/zeiss/resources/checkpoints/checkpoint.hdf5"
+print("Loading trained model", checkpoint_path)
+
+model = load_model(checkpoint_path)
+data_path = "/Users/Moritz/Desktop/zeiss/data/"
+# Load A - Channel
+
+# only loading the last file
+jaccard = []
+total_true_positives = []
+total_false_positives = []
+total_false_negatives = []
+true_totals = []
+dice_coef_loss_list = []
+
+positive_cells = np.empty([0,bounding_box_size,bounding_box_size,4])
+negative_cells = np.empty([0,bounding_box_size,bounding_box_size,4])
+
+
+for file_path in sorted(glob.glob(data_path + dataset + "0*_v2/"))[:-1]:
+    for csv_path in sorted(glob.glob(file_path + "*.csv")):
+        print(csv_path)
+        all_annotation_txt = np.genfromtxt(csv_path, dtype = 'str', comments='#', delimiter="',\n'", skip_header=0, skip_footer=0, converters=None, missing_values=None, filling_values=None, usecols=None, names=None, excludelist=None, deletechars=None, replace_space='_', autostrip=False, case_sensitive=True, defaultfmt='f%i', unpack=None, usemask=False, loose=True, invalid_raise=True, max_rows=None, encoding='bytes')
+        image = misc.imread(csv_path.replace(".csv", ".png"))/255
+        mask = misc.imread(csv_path.replace(".csv", ".jpg"))/255
+        cells_in_image = []
+        better_mask = np.zeros_like(image[:,:,0])
+        for cell_string in np.nditer(all_annotation_txt):
+            cell_array = np.fromstring(np.array2string(cell_string).strip("'"), dtype=int, sep=",")
+            cell_array = np.flip(np.reshape(cell_array, (-1,2)),1)
+            cells_in_image.append(cell_array)
+            better_mask[cell_array[:,0],cell_array[:,1]]=1
+            del cell_array
+        if plot:
+            fig, axs = plt.subplots(2, 2)
+            axs[0, 0].imshow(image)
+            axs[0, 1].imshow(better_mask)
+            axs[1, 0].imshow(image)
+            axs[1, 1].imshow(better_mask[550:640,])
+            plt.show()
+            code.interact(local=dict(globals(), **locals()))
+
+
+    #### Prediction ####
+        margin = np.round(bounding_box_size/2).astype(int)
+        pad_image = np.pad(image[:,:,:3], ((margin,margin),(margin,margin),(0,0)), mode="constant", constant_values=0)
+        pad_image = np.swapaxes(pad_image, 0,2)
+        yields = np.zeros_like(image[:,:,0])
+        for x in range(0, image.shape[0], stepSize):
+            for y in range(0, image.shape[1], stepSize):
+                yields[x,y] = model.predict(np.expand_dims(pad_image[:,x:x+2*margin,y:y+2*margin].astype('float32'), axis=0), batch_size=1, verbose=3)[0][1]
+        score_per_image = dice_coef_loss_here(better_mask, yields)
+        dice_coef_loss_list.append(score_per_image)
+    #### Morphological Operations ####
+        yields[yields<0.99]=0
+        yields[yields>=0.99]=1
+        kernel = np.ones((stepSize+2,stepSize+2),np.uint8)
+        dilation = cv2.dilate(yields,kernel,iterations = 1)
+        #opening = cv2.morphologyEx(yields, cv2.MORPH_OPEN, kernel)
+        #closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+
+        #### Evaluation ####
+        base_array, num_features = nd.label(dilation)
+        indices = np.unique(base_array, return_counts=True)
+        vals = []
+        cell_coords = []
+        for i in range(1, len(indices[1])):
+            if (indices[1][i] < minimum_cellsize) or (indices[1][i] > maximum_cellsize):
+                base_array[base_array==i]=0
+        base_array[base_array>=.1]=1
+        discard_array, num_cells = nd.label(base_array)
+        better_mask = np.swapaxes(better_mask,0,1)
+        overlap = base_array*better_mask
+        discard_array, true_positives = nd.label(overlap)
+        indices2 = np.unique(discard_array, return_counts=True)
+        positive_centers = np.empty([0,0])
+        for i in range(1, len(indices2[1])):
+            x_center = np.round(np.mean(np.argwhere(discard_array==i)[:,0])).astype(int)
+            y_center = np.round(np.mean(np.argwhere(discard_array==i)[:,1])).astype(int)
+            code.interact(local=dict(globals(), **locals()))
+            # positive_centers = np.concatenate((positive_centers, np.array([x_center, y_center]), axis=0))
+        false_positives = np.max(num_cells - true_positives,0)
+        false_negatives = np.max(len(cells_in_image)-true_positives,0)
+        #return average per image
+        score_per_image = jaccard_similarity_score(better_mask.flatten(), yields.flatten())
+        jaccard.append(score_per_image)
+        total_true_positives.append(true_positives)
+        total_false_positives.append(false_positives)
+        total_false_negatives.append(false_negatives)
+        true_totals.append(len(cells_in_image))
+
+
+
+code.interact(local=dict(globals(), **locals()))
+
+print("Mean Dice Loss")
+mean_dice_loss = np.mean(dice_coef_loss_list)
+print(mean_dice_loss)
+
+print("True Positives")
+print(total_true_positives)
+print(np.mean(total_true_positives))
+
+print("False Positives")
+print(total_false_positives)
+
+print("False Negatives ")
+print(total_false_negatives)
+
+tp = np.sum(total_true_positives)
+fn = np.sum(total_false_negatives)
+fp = np.sum(total_false_positives)
+total = np.sum(true_totals)
+precision = tp/(tp+fp)
+recall = tp/(tp+fn)
+f1 = 2*(precision*recall)/(precision+recall)
---
+++ README.md
@@ -0,0 +1,5 @@
+Ideas for Improvement:
+
+1. train a classifier to distinguish TPs + FNs and FPs based on Segmentation results on Train/Val Data. This should improve Precision and hopefully reduce false positives. - include probability map.
+
+2.  
